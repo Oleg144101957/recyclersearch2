167d317cba3b1268926d19260356dbf5f394ae35
OlegV <vishnevskiyoleg@gmail.com>
2022-09-04 11:43:25 +0300

    second

--- app/src/main/java/com/example/recyclersearch2/MainActivity.kt
+++ app/src/main/java/com/example/recyclersearch2/MainActivity.kt
@@ -12,6 +12,8 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.recyclersearch2.databinding.ActivityMainBinding
 
+
+//https://medium.com/@dracveg/%D0%BF%D0%BE%D0%B8%D1%81%D0%BA-%D0%BF%D0%BE-%D1%81%D0%BE%D0%B4%D0%B5%D1%80%D0%B6%D0%B8%D0%BC%D0%BE%D0%BC%D1%83-%D0%B2-recyclerview-fca7653f8e45
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
